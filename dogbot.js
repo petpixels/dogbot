4310b95f4e578c703bc88b7b145dd921408502d4
root <Alan Underwood>
2019-03-29 00:31:35 +0000

    Fixed connection issues with mongo and moved common functions into dog_functions

--- dogbot.js
+++ dogbot.js
@@ -24,22 +24,8 @@ var Document   = dclassify.Document
 // create a classifier
 var classifier
 var sentiment = new Sentiment()
-var sentiment_options = {
-	//applyInverse: true
-	extras: {
-		'🐶': 3,
-		'💩': -5,
-		'🦆': -5,
-		'🍑': -5,
-		'🐈': -5,
-		'🍍': -5,
-		'cat': -5,
-		'cats': -5,
-		'dog': 1,
-		'no': 0,
-		'agree': 5
-	}
-}
+var sentiment_options = require("./conf/sentiment.json")
+console.log(sentiment_options)
 
 //var global_training = []
 var global_training_good = []
@@ -48,20 +34,23 @@ var global_training_bad = []
 loadTraining()
 
 client.on('ready', () => {
-	classifierTrain()
-	var sayHello = false
+	var sayHello = true
 
-	dog.name("Dog")
-	dog.default_reply("Bark")
-	dog.keywords("dog,pup")
-	dog.rating("G")
-	dog.odds(.5)
+	dog.bot_name = "Dog"
+	dog.bot_reply = "Bark"
+	dog.bot_keywords = "dog,pup"
+	dog.bot_rating = "G"
+	dog.bot_odds = .5
+	dog.bot_channel = chan_dogbot
+	dog.bot_platform = "discord"
 	dog.log("Connected as " + client.user.tag)
+
+	console.log(dog)
 
 	// locate dogbot channel
 	client.guilds.forEach((guild) => {
 		guild.channels.forEach((channel) => {
-			console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+			//console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
 			if (channel.name.includes("dogbot")) {
 				// set dogbot channel
 				dog.bot_channel = channel.id
@@ -126,7 +115,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 
 	// dog log based on emoji
 	if (dogSentiment) {
-		var db_reply = dogReply(reaction.message.channel.id,reaction.emoji.name,reaction.message.author.id)
+		var db_reply = dog_functions.reply(dog,classifier,sentiment,reaction.message.channel.id,reaction.emoji.name,reaction.message.author.id)
 
 		if (db_reply) {
 			var lowerCaseReply = db_reply.toLowerCase()
@@ -139,7 +128,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 
 		if (dogSentiment.score > 0) {
 			// console.log("logging good")
-			dogTrain(reaction.message.author.id,dogSentiment.score)
+			dog_functions.train(dog,reaction.message.author.id,dogSentiment.score)
 
 			//good.info(logString)
 
@@ -151,7 +140,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 		}
 		if (dogSentiment.score < 0) {
 			// console.log("logging bad")
-			dogTrain(reaction.message.author.id,dogSentiment.score)
+			dog_functions.train(dog,reaction.message.author.id,dogSentiment.score)
 			//bad.info(logString)
 		}
 
@@ -166,6 +155,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 				// send message to the channel
 				if ((!(silent)) && (!(dog_functions.isBanned(reaction.message.channel.id)))) {
 					var randomBark = Math.random()
+					// clip happy output
 					if (dogSentiment.score > 0) {
 						if (randomBark > .25) {
 							channel.send(db_reply)
@@ -178,6 +168,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 		  }
 		})
 		.catch(console.error)
+
 
 	}
 })
@@ -208,7 +199,8 @@ client.on('message', (receivedMessage) => {
 				// don't log the dog
 				// ignore bots for now
 				var db_input = receivedMessage.content.toLowerCase()
-				var db_reply = dogReply(receivedMessage.channel.id,db_input,receivedMessage.author.id)
+				console.log(classifier)
+				var db_reply = dog_functions.reply(dog,classifier,sentiment,receivedMessage.channel.id,db_input,receivedMessage.author.id)
 
 				// dog log based on replies
 				if (db_reply) {
@@ -219,7 +211,7 @@ client.on('message', (receivedMessage) => {
 
 					if (lowerCaseReply.includes("woo")) {
 						//console.log("logging good")
-						dogTrain(receivedMessage.author.id,1)
+						dog_functions.train(dog,receivedMessage.author.id,1)
 
 						//good.info(logString)
 
@@ -231,7 +223,7 @@ client.on('message', (receivedMessage) => {
 					}
 					if ((lowerCaseReply.includes("grr")) || (lowerCaseReply.includes("bark"))) {
 						//console.log("logging bad")
-						dogTrain(receivedMessage.author.id,-1)
+						dog_functions.train(dog,receivedMessage.author.id,-1)
 
 						//bad.info(logString)
 
@@ -263,9 +255,10 @@ client.on('message', (receivedMessage) => {
 					if (!(silent)) {
 						receivedMessage.channel.fetchMessages({ limit: 1 }).then(messages => {
 							lastMessage = messages.first();
-							if (!lastMessage.author.bot) {
+							if (!(lastMessage.author.bot)) {
 								// send message to the channel
 								receivedMessage.channel.send(db_reply)
+								silent = true
 							}
 						})
 						.catch(console.error)
@@ -277,9 +270,10 @@ client.on('message', (receivedMessage) => {
 					if (chance < .05) {
 						receivedMessage.channel.fetchMessages({ limit: 1 }).then(messages => {
 							lastMessage = messages.first();
-							if (!lastMessage.author.bot) {
+							if (!(lastMessage.author.bot)) {
 								// send message to the channel
 								receivedMessage.channel.send("Bark!")
+								silent = true
 							}
 						})
 						.catch(console.error)
@@ -328,211 +322,19 @@ client.on('message', (receivedMessage) => {
 
 })
 
-function dogReply(channel,input,user) {
-	var db_input = input.toLowerCase()
-
-	//console.log("Channel: " + channel)
-	//console.log("User: " + user)
-
-	var testDoc = new Document('testDoc', user)
-  var dogAnalysis = classifier.classify(testDoc)
-
-	var dogLikesUser = dogAnalysis.category
-	//console.log(dogLikesUser)
-
-	var retString = dog.bot_reply
-	var silent = false
-
-	var db_output = []
-
-	// incoming message cannot be blank
-	if (db_input) {
-		var dogSentiment = sentiment.analyze(db_input, sentiment_options)
-		//console.log(dogSentiment)
-
-		//var dogString = "Bark!"
-		var wordScore = 0
-		var bias = dogBias(user)
-
-		// negative emotions
-		if (dogSentiment.score < 0) {
-			dogString = "Grrr"
-			wordScore = (dogSentiment.score * -1)
-		}
-
-		//positive emotions
-		if (dogSentiment.score > 0) {
-			dogString = "Woo"
-			wordScore = dogSentiment.score
-		}
-
-		if (wordScore) {
-
-			// positivity is amplified by a
-			// positive dog affinity while
-			var biasScore = wordScore
-			if (bias.dog_affinity == "good") {
-				// word is positive
-				// dog likes you so positive effect is amplified
-				if (dogSentiment.score > 0) {
-					biasScore = dogSentiment.score + bias.dog_bias
-				}
-				// word is negative
-				// dog likes you, negative effect is softened
-				if (dogSentiment.score < 0) {
-					biasScore = dogSentiment.score + bias.dog_bias
-				}
-			}
-			// negativity is amplified by a
-			// negative dog affinity
-			if (bias.dog_affinity == "bad") {
-				// word is positive
-				if (dogSentiment.score > 0) {
-					// dog dislikes you so positive effect is weakened
-					biasScore = dogSentiment.score - bias.dog_bias
-				}
-				// word is negative
-				if (dogSentiment.score < 0) {
-					biasScore = dogSentiment.score - bias.dog_bias
-				}
-			}
-
-			// inverts to positive
-			if (biasScore < 0) { biasScore *= -1 }
-
-			//console.log("wordScore: " + wordScore)
-			// console.log("biasScore: " + biasScore)
-			//console.log("Bias: " + bias.dog_bias)
-			//console.log("Affinity: " + bias.dog_affinity)
-
-			var range_score = 0
-
-			var dog_total = biasScore
-			var dog_like = dogSentiment.score
-			if (dog_total < 0) { dog_total *= -1 } // if negative make positive
-			if (dog_like < 0) { dog_like *= -1 } // if negative make positive
-
-			//console.log("Range total: " + dog_total)
-
-			for (var i = 0; i < dog_total; i++) {
-				if (!(i % 5)) { // divisible by 5
-					//console.log("modulo")
-					range_score++
-				}
-			}
-
-			range_score-- // subtract one to balance the zero
-			console.log("Range total: " + range_score)
-
-			// dog bias adds one or more exclamation points
-			retString = dogString
-
-			var randomBark = Math.random()
-			if (randomBark > .75) {
-				if (dogSentiment.score < 0) {
-					retString = "Bark!"
-				}
-			}
-
-			var tmpString = retString.toLowerCase()
-			for (var i = 0; i < range_score - 1; i++) {
-				if (tmpString.startsWith("gr")) {
-					retString += "r"
-				}
-				if (tmpString.startsWith("wo")) {
-					retString += "o"
-				}
-				if (tmpString.startsWith("bark")) {
-					retString += "!"
-				}
-			}
-
-			// excited dog
-			if (tmpString.startsWith("woo")) { retString += "f" } // close the string if necessary
-			if ((tmpString.startsWith("bark")) && (range_score >= 1)) { retString += "!" }
-			if ((tmpString.startsWith("woo")) && (range_score >= 2)) { retString += "!" }
-			if ((tmpString.startsWith("grr")) && (range_score <= 2)) { retString += "..." }
-			if (range_score >= 3) { retString = retString.toUpperCase() }
-
-			if ((retString) && (!(silent))) {
-				return retString
-			}
-
-			// log suppressed message
-			if (retString && (silent)) {
-				// receivedMessage.channel.id
-				//dog.log("<" + "channel_id" + "> @dogbot: (Message Suppressed) " + retString)
-			}
-		}
-	}
-}
-
-function classifierTrain() {
-	// train the classifier
-
-	// create some test items (name, array of characteristics)
-	var item_good = new Document('itemA', global_training_good)
-	var item_bad = new Document('item1', global_training_bad)
-
-	// create a DataSet and add test items to appropriate categories
-	// this is 'curated' data for training
-	var data = new DataSet()
-	data.add('good', [item_good])
-	data.add('bad', [item_bad])
-
-	// an optimisation for working with small vocabularies
-
-	classifier = new Classifier()
-	classifier.train(data)
-
-	return classifier
-}
-
-
-function dogBias(user) {
-	var dog_likes_user = "good" // the dog likes you by default
-	var dog_bias = 0
-
-	// check to see if the dog likes the user
-	var testDoc = new Document('testDoc', user)
-  var dogAnalysis = classifier.classify(testDoc)
-
-	dog_likes_user = dogAnalysis.category
-	dog_bias = Math.round((dogAnalysis.probability * 10))
-
-	var dog_emo = 0
-	if (dog_likes_user == "bad") {
-		dog_emo = dog_bias * -1
-	}
-
-	if (dog_likes_user == "good") {
-		dog_emo = dog_bias
-	}
-
-	var ret_val = {}
-	ret_val.dog_bias = dog_emo
-	ret_val.dog_affinity = dog_likes_user
-
-	return ret_val
-}
 
 function loadTraining() {
-	var retVal
-	console.log("Loading training data")
-	MongoClient.connect(db_url, function(err, db) {
-
-		var dog_db = db.db("dogbot")
-		var dog_sentiment = dog_db.collection("sentiment")
-
-		//user_training.find().toArray(function(err, items) {})
-
-		dog_sentiment.find({},{ user:1,sentiment:1, _id:0}).toArray(function(err, result) {
-    	if (err) throw err
-
-			//console.log(tmp.user)
-
+	var query = { }
+	var formatting = { user:1,sentiment:1, _id:0 }
+	console.log('here')
+		var initializePromise = dog.getDataMongo("dogbot","sentiment",query,formatting)
+		initializePromise.then(function(result) {
+			console.log("Dog training loaded");
+			// Use user details from here
+			//console.log(catsMeow)
 			result.forEach(function(item) {
 				//console.log(item.user + " : " + item.sentiment)
+				//console.log(item)
 
 				if (item.sentiment < 0) {
 					//console.log("BAD")
@@ -548,37 +350,14 @@ function loadTraining() {
 					}
 				}
 			})
-	    db.close()
-	  })
-	})
-}
 
-function dogTrain(user, sentiment) {
-	console.log(user)
-	console.log(sentiment)
+			classifier = dog_functions.classifierTrain(global_training_good,global_training_bad)
 
-	if (user) {
-		if (sentiment) {
-
-			MongoClient.connect(db_url, function(err, client) {
-				if (err) throw err
-
-				//var dictionary_db = db.db("emuji")
-				const collection = client.db("dogbot").collection("sentiment")
-				var dogLike = {}
-				dogLike.user = user
-				dogLike.sentiment = sentiment
-
-				var result = collection.insertOne(dogLike, function(err,result) {
-					if (err) throw err
-
-					//console.log(mongo_library)
-					console.log("Dog trained: " + dogLike.user + " : " + dogLike.sentiment)
-					return
-				})
-			})
-		}
-	}
+			return result
+			resolve(result)
+		}, function(err) {
+				console.log(err);
+		})
 }
 
 client.login(bot_secret.bot_secret_token)
